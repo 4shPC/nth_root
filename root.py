7d83f017b047a5a8d4504c32bae272b57cc52fbc
GoodGuyAsh <67311318+GoodGuyAsh@users.noreply.github.com>
2022-04-24 09:02:59 +0530

    Added the main function
    capable of finding roots

---
+++ root.py
@@ -0,0 +1,14 @@
+class Root_:
+    def __init__(self, root_, num):
+        self.root_ = root_
+        self.num = num
+        self.x1 = self.num/3
+        self.x2 = (1/self.root_) * (((self.root_ - 1)*self.x1) + (self.num/(self.x1**(self.root_ - 1))))
+
+    def func_root_(self):
+        return (1/self.root_) * (((self.root_ - 1)*self.x2) + (self.num/(self.x2**(self.root_ - 1))))
+
+    def root(self):
+        for i in range(10):
+            self.x2 = self.func_root_()
+            return self.x2
